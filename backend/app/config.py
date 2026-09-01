c1e7834fbc6f1c685af92d9b882f88ad46b06ace
Siddhartha Dey Chetia <excutioner01@gmail.com>
2026-09-02 02:11:08 +0530

    Add backend configuration

---
+++ backend/app/config.py
@@ -0,0 +1,8 @@
+class Config:
+    BASE_URL = "https://delhihighcourt.nic.in"
+    SEARCH_PATH = "/app/get-case-type-status"
+
+    PAGE_LOAD_TIMEOUT = 30
+    FORM_WAIT_TIMEOUT = 10
+
+    RATE_LIMIT_SECONDS = 2
